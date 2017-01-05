56d64f961ec8fd67114bdaf4d075b5af7343220c
anh.tranquoc <Anh.TranQuoc@harveynash.vn>
2017-01-05 15:53:57 +0700

    utf-8 enabled

--- modules/others.el
+++ modules/others.el
@@ -31,6 +31,14 @@
 ;; Overrite/delete selected region
 (delete-selection-mode t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Encoding
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
+(setq-default default-buffer-file-coding-system 'utf-8-unix)
 
 ;; ---------------------------------------------------------------------------
 ;; handy functions
