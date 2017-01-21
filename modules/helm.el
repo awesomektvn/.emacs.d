08d0acd506de9cc7fa327524942935d204ed9b45
anhnhoktvn <anhnhoktvn@gmail.com>
2017-01-21 19:25:04 +0700

    helm updated

--- modules/helm.el
+++ modules/helm.el
@@ -18,19 +18,30 @@
   (global-set-key (kbd "C-x f") 'helm-ls-git-ls)
   (global-set-key (kbd "C-x b") 'helm-buffers-list)
   ;;  (global-set-key (kbd "C-x C-b") 'helm-bookmarks)
-  
+
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+  (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+        helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+        helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+        helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+        helm-ff-file-name-history-use-recentf t
+        helm-echo-input-in-header-line t)
+
   
   (setq helm-recentf-fuzzy-match t)
   (setq helm-buffers-fuzzy-matching t)
   (setq helm-locate-fuzzy-match t)
   (setq helm-M-x-fuzzy-match t)
 
-  (defun fu/helm-find-files-navigate-forward (orig-fun &rest args)
-    (if (file-directory-p (helm-get-selection))
-	(apply orig-fun args)
-      (helm-maybe-exit-minibuffer)))
-  (advice-add 'helm-execute-persistent-action :around #'fu/helm-find-files-navigate-forward)
-  (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
+  ;; (defun fu/helm-find-files-navigate-forward (orig-fun &rest args)
+  ;;   (if (file-directory-p (helm-get-selection))
+  ;;       (apply orig-fun args)
+  ;;     (helm-maybe-exit-minibuffer)))
+  ;; (advice-add 'helm-execute-persistent-action :around #'fu/helm-find-files-navigate-forward)
+  ;; (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
 
   (defun fu/helm-find-files-navigate-back (orig-fun &rest args)
     (if (= (length helm-pattern) (length (helm-find-files-initial-input)))
--- modules/others.el
+++ modules/others.el
@@ -60,8 +60,32 @@
 (setq-default default-buffer-file-coding-system 'utf-8-unix)
 
 ;; ---------------------------------------------------------------------------
+;; Shell
+;; ---------------------------------------------------------------------------
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+;; ---------------------------------------------------------------------------
 ;; handy functions
 ;; ---------------------------------------------------------------------------
+
 (defun insert-date (prefix)
   "Insert the current date. With prefix-argument, use ISO format. With
    two prefix arguments, write out the day and month name."
