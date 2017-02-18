293fe958540481ed6bc016c3ad6ccaccda1393be
anhnhoktvn <anhnhoktvn@gmail.com>
2017-02-18 20:28:30 +0700

    ebc update

--- packages/ecb.el
+++ packages/ecb.el
@@ -7,7 +7,6 @@
   (setq ecb-windows-width 0.20)
   (setq ecb-compile-window-height nil)
   ;; (defvar ecb-examples-bufferinfo-buffer-name nil "otherwise it complains.")
-  ;; (setq ecb-compile-window-height nil)
   ;; (setq ecb-display-news-for-upgrade nil)
   ;; ;;(setq ecb-display-upgraded-options nil)
   (setq ecb-tip-of-the-day nil)
