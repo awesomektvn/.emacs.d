3d6ef8131560d959796c26070cb3d4b09db5e617
anh.tranquoc <Anh.TranQuoc@harveynash.vn>
2017-02-20 18:03:49 +0700

    org agenda

--- init.el
+++ init.el
@@ -50,27 +50,5 @@
 ;; ---------------------------------------------------------------------------
 ;; Generated code
 ;; ---------------------------------------------------------------------------
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(custom-safe-themes
-   (quote
-    ("d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
- '(ecb-layout-name "left10")
- '(ecb-options-version "2.50")
- '(org-agenda-files (quote ("d:/test.org")))
- '(org-todo-keywords (quote ((sequence "TODO(t)" "WORKING(w)" "DONE(d)"))))
- '(package-selected-packages
-   (quote
-    (ecb sr-speedbar undo-tree bm key-chord restart-emacs company nlinum hippie-exp-ext expand-line move-text rainbow-delimiters comment-dwim-2 smartparens-config smartparens restclient discover-my-major help-fns+ which-key browse-kill-ring change-inner ace-link avy-zap ace-window paradox emmet-mode ivy-hydra hydra avy counsel ivy js2-mode blackboard-theme git-gutter magit powerline projectile yasnippet web-mode use-package multiple-cursors expand-region bookmark+ ace-jump-mode)))
- '(paradox-automatically-star t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
--- packages/org.el
+++ packages/org.el
@@ -3,3 +3,6 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/school.org"
+                             "~/org/home.org"))
