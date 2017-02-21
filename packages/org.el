5d34912f109eed02a81976acc52003c050668805
anh.tranquoc <Anh.TranQuoc@harveynash.vn>
2017-02-21 10:22:38 +0700

    org updated

--- packages/org.el
+++ packages/org.el
@@ -3,4 +3,8 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
-(custom-set-variables '(org-agenda-files (quote ("~/org"))))
+
+(custom-set-variables '(org-agenda-files (quote ("~/org")))
+                      '(org-todo-keywords (quote ((sequence "TODO(t)" "WIP(w)" "DONE(d)"))))
+                      )
+
