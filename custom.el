494de5ec75908f6f14232b762b8f4b6b19fd2a76
anhnhoktvn <anhnhoktvn@gmail.com>
2017-02-23 01:37:50 +0700

    org updated

--- custom.el
+++ custom.el
@@ -9,7 +9,6 @@
    (quote
     ("d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
  '(ecb-layout-name "left10")
- '(org-todo-keywords (quote ((sequence "TODO(t)" "WORKING(w)" "DONE(d)"))))
  '(package-selected-packages
    (quote
     (ecb sr-speedbar undo-tree bm key-chord restart-emacs company nlinum hippie-exp-ext expand-line move-text rainbow-delimiters comment-dwim-2 smartparens-config smartparens restclient discover-my-major help-fns+ which-key browse-kill-ring change-inner ace-link avy-zap ace-window paradox emmet-mode ivy-hydra hydra avy counsel ivy js2-mode blackboard-theme git-gutter magit powerline projectile yasnippet web-mode use-package multiple-cursors expand-region bookmark+ ace-jump-mode)))
